f30acbc03653eba0a856a12fbd8b3eaee326ef87
Alan Sousa <alanssrv@outlook.com>
2025-04-01 07:41:02 -0300

    feat: descricao do script do banco de dados

--- database/script.sql
+++ database/script.sql
@@ -1,6 +1,8 @@
+-- Para o teste foi criada uma base de dados com nome CTSCompass
 USE CTSCompass
 GO
 
+-- Criação da tabela de Produtos com os campos seguindo a descrição do teste
 CREATE TABLE Produtos (
 	Id INT IDENTITY(1,1) PRIMARY KEY,
 	Nome VARCHAR(50),
@@ -8,6 +10,7 @@ CREATE TABLE Produtos (
 	Quantidade INT
 )
 
+-- Inserção de registros seguindo a descrição do teste
 INSERT INTO Produtos VALUES
 	('Produto A', 10.50, 100),
 	('Produto B', 20, 200),
